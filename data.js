e6594e133ccf65a0201725414dc6079f4948955b
adraalhafid36 <adraalhafid36@gmail.com>
2026-03-03 04:31:29 +0700

    Add portfolioData object with cartoon and character arrays

---
+++ data.js
@@ -0,0 +1,27 @@
+const portfolioData = {
+
+cartoon: [
+{
+file:"devil.jpg",
+title:"Devil Mascot",
+description:"Retro cartoon mascot illustration."
+}
+],
+
+character: [
+{
+file:"warrior.jpg",
+title:"Warrior Character",
+description:"Strong personality character design."
+}
+],
+
+children: [
+{
+file:"forest-story.jpg",
+title:"Forest Adventure",
+description:"Children book storytelling scene."
+}
+]
+
+};
